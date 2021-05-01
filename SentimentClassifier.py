dd6e0d38fd66cdf241e2169d7f9941704c93696d
Gabriel Rębacz <gabriel.rebacz@enigma.com.pl>
2021-05-01 16:46:23 +0200

    Szkielet do dostosowania

---
+++ SentimentClassifier.py
@@ -0,0 +1,19 @@
+from torch import nn
+from transformers import BertModel
+
+
+class SentimentClassifier(nn.Module):
+
+    def __init__(self, n_classes, pretrained_model_name):
+        super(SentimentClassifier, self).__init__()
+        self.bert = BertModel.from_pretrained(pretrained_model_name)
+        self.drop = nn.Dropout(p=0.3)
+        self.out = nn.Linear(self.bert.config.hidden_size, n_classes)
+
+    def forward(self, input_ids, attention_mask):
+        _, pooled_output = self.bert(
+            input_ids=input_ids,
+            attention_mask=attention_mask
+        )
+        output = self.drop(pooled_output)
+        return self.out(output)
